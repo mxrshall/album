dee4a7f5cb73ce2e03f723b0fff002b8d8784504
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-11-28 11:05:18 +0100

    Vytvorenie fotky

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Photo from './components/Photo.vue'
+</script>
+
+<template>
+  <div class="w-full h-full flex items-center justify-center py-10">
+    <Photo />
+  </div>
+</template>
+
+
---
+++ src/components/Photo.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-[75%] flex flex-col items-center justify-center bg-gray-100 p-5 text-center shadow-2xl rotate-[-5deg]">
+        <div class="w-full h-[25vh]">
+            <img src="../assets/images/photo1.jpeg" alt="APE" class="w-full h-full object-cover"/>
+        </div>
+        <h1 class="text-2xl font-semibold mt-5">APE halloween</h1>
+        <p class="text-xl font-base mt-1">31.okt 2024</p>
+    </div>
+</template>
