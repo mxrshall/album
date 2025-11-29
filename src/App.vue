2c1db6455358ef2f04dc1b4a5c7cbdadf4d8c400
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-11-29 19:01:05 +0100

    Pridanie dynamickeho passovanie dat

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,26 @@ import photo4 from './assets/images/photo4.jpeg'
 import photo5 from './assets/images/photo5.jpeg'
 import photo6 from './assets/images/photo6.jpeg'
 
+const photos = [
+  { title: "OOZI for students", date: "25.nov 2025", image: photo4, position: 5 },
+  { title: "Východňarovica", date: "11.nov 2025", image: photo3, position: -5 },
+  { title: "10Bar Rave", date: "15.okt 2025", image: photo5, position: -5 },
+  { title: "Rozlúčka s letom Chtelnica", date: "6.sep 2025", image: photo6, position: 5 },
+  { title: "Privítanie leta Chtelnica", date: "28.máj 2025", image: photo2, position: 5 },
+  { title: "APE halloween", date: "31.okt 2024", image: photo1, position: -5 }
+]
 </script>
-
 
 <template>
   <div class="w-full h-full flex flex-col items-center justify-center py-32">
     <Navigation />
-    <Photo title="OOZI for students" date="25.nov 2025" :image="photo4" position="5" />
-    <Photo title="Východňarovica" date="11.nov 2025" :image="photo3" position="-5" />
-    <Photo title="10Bar Rave" date="15.okt 2025" :image="photo5" position="-5" />
-    <Photo title="Rozlúčka s letom Chtelnica" date="6.sep 2025" :image="photo6" position="5" />
-    <Photo title="Privítanie leta Chtelnica" date="28.máj 2025" :image="photo2" position="5" />
-    <Photo title="APE halloween" date="31.okt 2024" :image="photo1" position="-5" />
+    <Photo 
+      v-for="(photo, index) in photos" 
+      :key="index"
+      :title="photo.title" 
+      :date="photo.date" 
+      :image="photo.image" 
+      :position="photo.position" 
+    />
   </div>
 </template>
