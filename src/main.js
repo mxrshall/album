868141b319deb8288fc37b4fa113c3051a0b948c
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-12-10 22:13:55 +0100

    Pridanie router

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import App from './App.vue'
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import { CoHamburgerMenu, MdOpeninnew } from "oh-vue-icons/icons";
 
+import router from "./router";
+
 addIcons(CoHamburgerMenu, MdOpeninnew);
 
 const app = createApp(App);
+app.use(router);
 app.component("v-icon", OhVueIcon);
 app.mount("#app");
