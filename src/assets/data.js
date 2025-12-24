a884a7b427a32eb79885127b710f451af0705259
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-12-25 00:33:24 +0100

    Pridanie novych fotiek

--- src/assets/data.js
+++ src/assets/data.js
@@ -13,20 +13,30 @@ import photo12 from '../assets/images/photo12.jpg'
 import photo13 from '../assets/images/photo13.jpg'
 import photo14 from '../assets/images/photo14.jpg'
 import photo15 from '../assets/images/photo15.jpg'
+import photo16 from '../assets/images/photo16.jpg'
+import photo17 from '../assets/images/photo17.jpg'
 
 import video1 from '../assets/video/video1.mp4'
 import video2 from '../assets/video/video2.mp4'
 import video3 from '../assets/video/video3.mp4'
+import video4 from '../assets/video/video4.mp4'
+import video5 from '../assets/video/video5.mp4'
+import video6 from '../assets/video/video6.mp4'
+import video7 from '../assets/video/video7.mp4'
+import video8 from '../assets/video/video8.mp4'
+import video9 from '../assets/video/video9.mp4'
+import video10 from '../assets/video/video10.mp4'
 
 export const photos = [
   { id: 1, title: "Bye Bye Semester MTF", date: "9.dec 2025", thumbnail: photo8, image: photo8, position: 5 },
-  { id: 2, title: "Goodbye Party UCM", date: "2.dec 2025", thumbnail: photo7, image: [photo7, photo9] },
-  { id: 3, title: "OOZI for students", date: "25.nov 2025", thumbnail: photo4, image: photo4, position: 5 },
-  { id: 4, title: "Východňarovica", date: "11.nov 2025", thumbnail: photo3, image: photo3, position: -5 },
+  { id: 2, title: "Goodbye Party UCM", date: "2.dec 2025", thumbnail: photo7, image: [photo7, photo9], position: -5 },
+  { id: 3, title: "OOZI for students", date: "25.nov 2025", thumbnail: photo4, image: [photo4, photo17], position: 5 },
+  { id: 4, title: "Východňarovica", date: "11.nov 2025", thumbnail: photo3, image: photo3, video: [video8, video9, video10], position: -5 },
   { id: 5, title: "Prvákovica semafor MTF", date: "4.nov 2025", thumbnail: photo10, image: [photo10, photo11, photo12, photo13, photo14], position: 5 },
   { id: 6, title: "10Bar Rave", date: "15.okt 2025", thumbnail: photo5, image: photo5, position: -5 },
-  { id: 7, title: "Open semester - Právnická", date: "30.sep 2025", thumbnail: photo15, image: photo15, video: [video1, video2, video3], position: 5 },
+  { id: 7, title: "Open zimného semestra - Právnická", date: "30.sep 2025", thumbnail: photo15, image: photo15, video: [video1, video2, video3], position: 5 },
   { id: 8, title: "Rozlúčka s letom Chtelnica", date: "6.sep 2025", thumbnail: photo6, image: photo6, position: -5 },
   { id: 9, title: "Privítanie leta Chtelnica", date: "28.jún 2025", thumbnail: photo2, image: photo2, position: 5 },
-  { id: 10, title: "APE halloween", date: "31.okt 2024", thumbnail: photo1, image: photo1, position: -5 }
+  { id: 10, title: "Open letného semestra - Právnická", date: "17.2.2025", thumbnail: photo16, image: photo16, video: [video4, video5, video6, video7], position: -5 },
+  { id: 11, title: "APE halloween", date: "31.okt 2024", thumbnail: photo1, image: photo1, position: 5 }
 ]
